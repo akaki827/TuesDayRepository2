2e2daad0a2df45795e47e39877f04da80418afb3
akaki827 <jagaimo287@gmail.com>
2026-06-30 16:03:28 +0900

    初期化

---
+++ 2026_6_30/ソース.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+double power(double x, int n);
+
+int main()
+{
+	double x;
+	double a = 3.2;
+	int b = 5;
+
+	x = power(a, b);
+	std::cout << "Result: " << x << std::endl;
+	return 0;
+}
+
+double power(double x, int n)
+{
+
+}
